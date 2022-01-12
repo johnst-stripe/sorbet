4e841f836ff7c717cee61f40c47c50bf376943fe
Nathan Froyd <froydnj@gmail.com>
2022-01-12 09:38:39 -1000

    change tokens to store `string_view`

--- parser/parser/include/ruby_parser/token.hh
+++ parser/parser/include/ruby_parser/token.hh
@@ -170,7 +170,7 @@ class token {
     token_type _type;
     size_t _start;
     size_t _end;
-    std::string _string;
+    std::string_view _string;
 
 public:
     token(token_type type, size_t start, size_t end, std::string_view str);
@@ -179,7 +179,7 @@ public:
     size_t start() const;
     size_t end() const;
     std::string_view view() const;
-    const std::string &asString() const;
+    std::string asString() const;
 };
 
 using token_t = token *;
